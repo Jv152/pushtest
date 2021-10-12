40179fff124822cdd16b974d3c6c3800589e00f3
USER <USER@MyMSI>
2021-10-12 15:56:08 +0700

    test

---
+++ src/main/java/Test/Weekday.java
@@ -0,0 +1,11 @@
+package Test;
+import java.util.Scanner;
+public class Weekday {
+    public static void main(String[] args) {
+        String[] days = {"Monday" , "Tuesday","Wednesday","Thursday","Friday","Saturday" , "Sunday"};
+        System.out.println("Number: ");
+        Scanner input = new Scanner(System.in);
+        int number = input.nextInt();
+        System.out.println(days[number]);
+    }
+}
